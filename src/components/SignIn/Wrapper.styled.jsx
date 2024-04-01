9129a13299e9905d76326dfa8bd088a14f0476e7
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-01 22:34:01 +0300

    Update Wrapper.styled.jsx
    fix element position betwen diff pages
    main - signin - signup

--- src/components/SignIn/Wrapper.styled.jsx
+++ src/components/SignIn/Wrapper.styled.jsx
@@ -1,14 +1,12 @@
-import styled from "styled-components";
-
-
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-min-height: 430px;
-@media screen and (min-width: 768px) {
-  min-height: 526px;
-}  
-@media screen and (min-width: 1440px) {
-  min-height: 100px;
-  min-width: 600px;
-} 
-`
+  min-height: 433px;
+  @media screen and (min-width: 768px) {
+    min-height: 525px;
+  }
+  @media screen and (min-width: 1440px) {
+    min-height: 100px;
+    min-width: 598px;
+  }
+`;
