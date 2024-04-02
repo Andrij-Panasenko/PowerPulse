ef6a5c24c81802687a83b862925bc5b5ebbb367d
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-02 22:24:01 +0300

    delete comments from Loader

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -8,14 +8,5 @@ export const LoaderWrapper = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  /* display: block;
-  width: 100px;
-  height: 20px;
-
-
-  position: absolute;
-  left: 0; */
-
-  /* transform: translate(100%,100%); */
 `;
 
