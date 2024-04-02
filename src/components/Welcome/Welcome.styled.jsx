0e2eb8fc6b3c87fb8645c575550c3e47618886dd
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-02 22:28:53 +0300

    formatting Welcome

--- src/components/Welcome/Welcome.styled.jsx
+++ src/components/Welcome/Welcome.styled.jsx
@@ -4,7 +4,6 @@ export const Container = styled.div`
   padding-bottom: 40px;
   padding-right: 20px;
   padding-top: 127px;
-  /* outline: 2px solid red; */
 
   @media screen and (min-width: 768px) {
     padding-bottom: 48px;
